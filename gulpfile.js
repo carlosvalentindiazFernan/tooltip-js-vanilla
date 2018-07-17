edfe9a0d5b3201161d3750590057c39ed4185b4b
CarlsKe <fernancarldiaz@outlook.com>
2018-07-17 16:16:52 -0500

    feat initial commit

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+
+gulp.task('connect', ()=>{
+  connect.server({
+    root: 'build',
+    livereload: true
+  });
+});
+
+
+
+gulp.task('html', function(){
+  return gulp.src('src/templates/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('build/'))
+    .pipe(connect.reload());
+  });
+
+
+
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/**/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('build/css'))
+    .pipe(connect.reload());
+
+  });
+ 
+  
+
+gulp.task('watch', function () {
+  gulp.watch('./src/sass/**/*.sass', ['sass']);
+  gulp.watch('./src/templates/*.pug',['html']);
+
+});
+
+
+gulp.task('default', [ 'watch' ,'connect' ]);
