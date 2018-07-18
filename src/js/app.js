b32736e38bfb7f607a5cb0e1bbf81d6e49e84c6c
CarlsKe <fernancarldiaz@outlook.com>
2018-07-18 16:44:30 -0500

    feat: added selector $

--- src/js/app.js
+++ src/js/app.js
@@ -1,78 +1,73 @@
-(function TooltipFucn(){
+(function TooltipFucn() {
     'use strict'
 
-    class Tooltip{
+    var $ = function (selector) {
 
-        constructor(title,decripcion,direccion,select){
-            this._title = title;
-            this._descripcion = decripcion;
-            this._direccion = direccion
-            this._select = select
+        if (!(this instanceof $)) {
+            return new $(selector);
         }
-    
-        createELement(){
-    
+
+
+        this._select = document.querySelectorAll(selector);
+        this._title = "";
+        this._descripcion = "";
+        this._onApprove = "";
+
+        $.prototype.showTooltip = function (obj) {
+
+            this._title = obj.title;
+            this._descripcion = obj.description;
+            this._onApprove = obj.onApprove;
+
+            this._select.forEach(function (element) {
+                CreateELementFunction(element)
+                element.addEventListener('mouseover', () => {
+                    let div = element.previousSibling.previousElementSibling;
+                    showElememnt(element)
+        
+                })
+            });
+        }
+
+        function showElememnt(element) {
+
+            let div = element.previousSibling.previousElementSibling;
+            console.log(div.className)
+            if (div.className === 'md-container-tootip-hide' || div.className === null) {
+                div.classList.remove('md-container-tootip-hide');
+                div.classList.add('md-container-tooltip-show-up');
+
+                setTimeout(() => {                    
+                    div.classList.remove('md-container-tooltip-show-up');
+                    div.classList.add('md-container-tootip-hide');
+                }, 600);
+            }
+
+
+        }
+
+        function CreateELementFunction(element, title, description) {
             let template = `
-                <div class="md-container-tootip-hide">
-                    <p class="title-tooltip">${this._title}</p>
-                    <p class="description-tooltip">${this._descripcion}</p>
-                </div>
-            `;
-            this._select.insertAdjacentHTML('beforebegin',template);    
-            this.showElememnt()
-        }
+                    <div class="md-container-tootip-hide">
+                        <p class="title-tooltip">${title}</p>
+                        <p class="description-tooltip">${description}</p>
+                    </div>
+                `;
 
-        showElememnt(){
-
-            this._select.addEventListener('mouseover',(e)=>{
-                let div = this._select.previousSibling.previousElementSibling;
-                console.log(div);
-                if(div.className === 'md-container-tootip-hide'){
-                    switch(this._direccion){
-                        case 'up':
-                            div.classList.remove('md-container-tootip-hide');
-                            div.classList.add('md-container-tooltip-show-up');
-
-                        break;
-                        case 'down':
-                            div.classList.remove('md-container-tootip-hide');
-                            div.classList.add('md-container-tooltip-show-down');
-
-                        break;
-                        case 'left':
-                            div.classList.remove('md-container-tootip-hide');
-                            div.classList.add('md-container-tooltip-show-left');
-                        break;
-                        case 'right':
-                            div.classList.remove('md-container-tootip-hide');
-                            div.classList.add('md-container-tooltip-show-right');
-
-                        break;
-                    }
-
-                    setTimeout(() => {
-                        div.classList.remove(div.className);
-                        div.classList.add('md-container-tootip-hide');
-                    }, 600);
-                }
-            })
+            element.insertAdjacentHTML('beforebegin', template);
 
         }
-    }
-    
-    let elements = document.querySelectorAll('[data-toggle="tooltip"]');
-    elements.forEach(e => getValues(e))
 
-    function getValues (element){
-        let title = element.dataset.title
-        let description = element.dataset.description
-        let direccion = element.dataset.direcion
-        let select = document.querySelector("#"+element.id)
-        let data = new Tooltip(title,description,direccion,select);
-        data.createELement();
+        return this;
 
     }
 
+
+    $('#button-left').showTooltip({
+        'title': 'demo',
+        'description': 'this  is a description',
+        'onApprove': 'messaje click'
+    });
 
 })();
 
